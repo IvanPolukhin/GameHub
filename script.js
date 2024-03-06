eb82a346f75ecf40f9618c4713149c15584045ea
Ivan Poluhin <ivan.poluhin.200407@gmail.com>
2024-03-06 21:28:59 +0200

    JS buttons

--- script.js
+++ script.js
@@ -22,15 +22,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // слайдер 
 document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".homePage_bnt");
     const slides = document.querySelectorAll(".slide");
     const prevSlideButton = document.querySelector(".prev-slide");
     const nextSlideButton = document.querySelector(".next-slide");
     const slideInterval = 5000;
     let currentIndex = 0;
     let slideIntervalId;
-    const prevSlideButton1 = document.querySelector(".prev-slide1");
-    const nextSlideButton1 = document.querySelector(".next-slide1");
 
     const showSlide = (index) => {
         slides.forEach((slide, i) => {
@@ -42,16 +39,6 @@ document.addEventListener("DOMContentLoaded", function () {
             dot.classList.toggle('focus', i === index);
         });
     };
-
-    const counterDots = document.querySelectorAll('.slider-counter div');
-    counterDots.forEach((dot, index) => {
-        dot.addEventListener('click', () => {
-            stopSlideShow();
-            currentIndex = index;
-            showSlide(currentIndex);
-            startSlideShow();
-        });
-    });
 
     const showNextSlide = () => {
         currentIndex = (currentIndex + 1) % slides.length;
@@ -84,18 +71,6 @@ document.addEventListener("DOMContentLoaded", function () {
         slide.addEventListener('mouseout', handleSlideMouseout);
     });
 
-    prevSlideButton1.addEventListener("click", () => {
-        stopSlideShow();
-        showPrevSlide();
-        startSlideShow();
-    });
-
-    nextSlideButton1.addEventListener("click", () => {
-        stopSlideShow();
-        showNextSlide();
-        startSlideShow();
-    });
-
     prevSlideButton.addEventListener("click", () => {
         stopSlideShow();
         showPrevSlide();
@@ -110,14 +85,60 @@ document.addEventListener("DOMContentLoaded", function () {
 
     showSlide(currentIndex);
 
-    buttons.forEach((button, index) => {
-        button.addEventListener("click", function () {
-            stopSlideShow();
-            currentIndex = index;
-            showSlide(currentIndex);
-            startSlideShow();
+    startSlideShow();
+});
+
+// слайдер для specialOffers
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".specialOffers-item");
+    const prevSlideButton = document.querySelector(".prev-slide1");
+    const nextSlideButton = document.querySelector(".next-slide1");
+    const slideInterval = 5000;
+    let currentIndex = 0;
+    let slideIntervalId;
+
+    const showSlide = (index) => {
+        slides.forEach((slide, i) => {
+            slide.style.display = i === index ? "flex" : "none";
         });
+    };
+
+    const counterDots = document.querySelectorAll('.dot1');
+    counterDots.forEach((dot, i) => {
+        dot.classList.toggle('focus', i === currentIndex)
     });
+
+    const showNextSlide = () => {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    };
+
+    const showPrevSlide = () => {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+    };
+
+    const startSlideShow = () => {
+        slideIntervalId = setInterval(showNextSlide, slideInterval);
+    };
+
+    const stopSlideShow = () => {
+        clearInterval(slideIntervalId);
+    };
+
+    prevSlideButton.addEventListener("click", () => {
+        stopSlideShow();
+        showPrevSlide();
+        startSlideShow();
+    });
+
+    nextSlideButton.addEventListener("click", () => {
+        stopSlideShow();
+        showNextSlide();
+        startSlideShow();
+    });
+
+    showSlide(currentIndex);
 
     startSlideShow();
 });

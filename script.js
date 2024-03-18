56fc6a8adb5c76ef732ba2b4a72d7693d8ebfdb0
Ivan Poluhin <ivan.poluhin.200407@gmail.com>
2024-03-18 21:59:26 +0200

    bug fixing

--- script.js
+++ script.js
@@ -1,188 +1,172 @@
-// Смена img 
 document.addEventListener("DOMContentLoaded", function () {
-    const sliderWrapper = document.querySelector(".slider-wrapper");
-    const slides = document.querySelectorAll(".slide");
+    const objectElement = document.querySelector('object[data="popup.html"]')
+    objectElement.addEventListener("load", function () {
+        // Смена img 
+        const sliderWrapper = document.querySelector(".slider-wrapper");
+        const slides1 = document.querySelectorAll(".slide");
 
-    slides.forEach((slide, index) => {
-        const mainGameImage = slide.querySelector(".left-side img");
-        const rightImages = slide.querySelectorAll(".right-side img");
-        const defaultMainImageSrc = mainGameImage.src;
+        slides1.forEach((slide, index) => {
+            const mainGameImage = slide.querySelector(".left-side img");
+            const rightImages = slide.querySelectorAll(".right-side img");
+            const defaultMainImageSrc = mainGameImage.src;
 
-        rightImages.forEach(img => {
-            img.addEventListener("mouseover", function () {
-                mainGameImage.src = this.src;
+            rightImages.forEach(img => {
+                img.addEventListener("mouseover", function () {
+                    mainGameImage.src = this.src;
+                });
+            });
+
+            sliderWrapper.addEventListener("mouseleave", function () {
+                mainGameImage.src = defaultMainImageSrc;
             });
         });
 
-        sliderWrapper.addEventListener("mouseleave", function () {
-            mainGameImage.src = defaultMainImageSrc;
+        // слайдер 
+        const prevSlideButton = document.querySelector(".prev-slide");
+        const nextSlideButton = document.querySelector(".next-slide");
+        const slides2 = document.querySelectorAll(".slide");
+        const slideInterval = 5000;
+        let currentIndex = 0;
+        let slideIntervalId;
+
+        const showSlide = (index) => {
+            slides2.forEach((slide, i) => {
+                slide.style.display = i === index ? "flex" : "none";
+            });
+
+            const counterDots = document.querySelectorAll('.slider-counter div');
+            counterDots.forEach((dot, i) => {
+                dot.classList.toggle('focus', i === index);
+            });
+        };
+
+        const showNextSlide = () => {
+            currentIndex = (currentIndex + 1) % slides2.length;
+            showSlide(currentIndex);
+        };
+
+        const showPrevSlide = () => {
+            currentIndex = (currentIndex - 1 + slides2.length) % slides2.length;
+            showSlide(currentIndex);
+        };
+
+        const startSlideShow = () => {
+            slideIntervalId = setInterval(showNextSlide, slideInterval);
+        };
+
+        const stopSlideShow = () => {
+            clearInterval(slideIntervalId);
+        };
+
+        const handleSlideMouseover = () => {
+            stopSlideShow();
+        };
+
+        const handleSlideMouseout = () => {
+            startSlideShow();
+        };
+
+        slides2.forEach(slide => {
+            slide.addEventListener('mouseover', handleSlideMouseover);
+            slide.addEventListener('mouseout', handleSlideMouseout);
         });
-    });
-});
 
-// слайдер 
-document.addEventListener("DOMContentLoaded", function () {
-    const slides = document.querySelectorAll(".slide");
-    const prevSlideButton = document.querySelector(".prev-slide");
-    const nextSlideButton = document.querySelector(".next-slide");
-    const slideInterval = 5000;
-    let currentIndex = 0;
-    let slideIntervalId;
-
-    const showSlide = (index) => {
-        slides.forEach((slide, i) => {
-            slide.style.display = i === index ? "flex" : "none";
+        prevSlideButton.addEventListener("click", () => {
+            stopSlideShow();
+            showPrevSlide();
+            startSlideShow();
         });
 
-        const counterDots = document.querySelectorAll('.slider-counter div');
-        counterDots.forEach((dot, i) => {
-            dot.classList.toggle('focus', i === index);
+        nextSlideButton.addEventListener("click", () => {
+            stopSlideShow();
+            showNextSlide();
+            startSlideShow();
         });
-    };
 
-    const showNextSlide = () => {
-        currentIndex = (currentIndex + 1) % slides.length;
         showSlide(currentIndex);
-    };
 
-    const showPrevSlide = () => {
-        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-        showSlide(currentIndex);
-    };
-
-    const startSlideShow = () => {
-        slideIntervalId = setInterval(showNextSlide, slideInterval);
-    };
-
-    const stopSlideShow = () => {
-        clearInterval(slideIntervalId);
-    };
-
-    const handleSlideMouseover = () => {
-        stopSlideShow();
-    };
-
-    const handleSlideMouseout = () => {
         startSlideShow();
-    };
 
-    slides.forEach(slide => {
-        slide.addEventListener('mouseover', handleSlideMouseover);
-        slide.addEventListener('mouseout', handleSlideMouseout);
-    });
+        // слайдер для specialOffers
+        const prevSlideButton1 = document.querySelector(".prev-slide1");
+        const nextSlideButton1 = document.querySelector(".next-slide1");
+        const slides3 = document.querySelectorAll(".specialOffers-item");
+        const slideInterval1 = 5000;
+        let currentIndex1 = 0;
+        let slideIntervalId1;
 
-    prevSlideButton.addEventListener("click", () => {
-        stopSlideShow();
-        showPrevSlide();
-        startSlideShow();
-    });
+        const showSlide1 = (index) => {
+            slides3.forEach((slide, i) => {
+                slide.style.display = i === index ? "flex" : "none";
+            });
 
-    nextSlideButton.addEventListener("click", () => {
-        stopSlideShow();
-        showNextSlide();
-        startSlideShow();
-    });
+            const counterDots = document.querySelectorAll('.dot1');
+            counterDots.forEach((dot, i) => {
+                dot.classList.toggle('focus', i === index);
+            });
+        };
 
-    showSlide(currentIndex);
+        const showNextSlide1 = () => {
+            currentIndex1 = (currentIndex1 + 1) % slides3.length;
+            showSlide1(currentIndex1);
+        };
 
-    startSlideShow();
-});
+        const showPrevSlide1 = () => {
+            currentIndex1 = (currentIndex1 - 1 + slides3.length) % slides3.length;
+            showSlide1(currentIndex1);
+        };
 
-// слайдер для specialOffers
-document.addEventListener("DOMContentLoaded", function () {
-    const slides = document.querySelectorAll(".specialOffers-item");
-    const prevSlideButton = document.querySelector(".prev-slide1");
-    const nextSlideButton = document.querySelector(".next-slide1");
-    const slideInterval = 5000;
-    let currentIndex = 0;
-    let slideIntervalId;
+        const startSlideShow1 = () => {
+            slideIntervalId1 = setInterval(showNextSlide1, slideInterval1);
+        };
 
-    const showSlide = (index) => {
-        slides.forEach((slide, i) => {
-            slide.style.display = i === index ? "flex" : "none";
+        const stopSlideShow1 = () => {
+            clearInterval(slideIntervalId1);
+        };
+
+        prevSlideButton1.addEventListener("click", () => {
+            stopSlideShow1();
+            showPrevSlide1();
+            startSlideShow1();
         });
 
-        const counterDots = document.querySelectorAll('.dot1');
-        counterDots.forEach((dot, i) => {
-            dot.classList.toggle('focus', i === index);
+        nextSlideButton1.addEventListener("click", () => {
+            stopSlideShow1();
+            showNextSlide1();
+            startSlideShow1();
         });
-    };
 
-    const showNextSlide = () => {
-        currentIndex = (currentIndex + 1) % slides.length;
-        showSlide(currentIndex);
-    };
+        showSlide1(currentIndex1);
 
-    const showPrevSlide = () => {
-        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-        showSlide(currentIndex);
-    };
+        startSlideShow1();
 
-    const startSlideShow = () => {
-        slideIntervalId = setInterval(showNextSlide, slideInterval);
-    };
+        // iconBasket
+        const basketIcon = document.querySelector('.basketIcon');
 
-    const stopSlideShow = () => {
-        clearInterval(slideIntervalId);
-    };
+        // Получаем все картинки в слайдере
+        const sliderImages = document.querySelectorAll('.slide img');
 
-    prevSlideButton.addEventListener("click", () => {
-        stopSlideShow();
-        showPrevSlide();
-        startSlideShow();
-    });
+        // Добавляем обработчик события клика для каждой картинки в слайдере
+        sliderImages.forEach(image => {
+            image.addEventListener('click', () => {
+                // Получаем информацию о выбранной игре
+                const gameTitle = image.closest('.slide').querySelector('.gameName').dataset.gameTitle;
+                const gamePrice = image.closest('.slide').querySelector('.slider_price').textContent;
 
-    nextSlideButton.addEventListener("click", () => {
-        stopSlideShow();
-        showNextSlide();
-        startSlideShow();
-    });
+                // Отображаем информацию о выбранной игре в окне
+                basketIcon.querySelector('.game_title').textContent = gameTitle;
+                basketIcon.querySelector('.panel_price-left').textContent = gamePrice;
 
-    showSlide(currentIndex);
-
-    startSlideShow();
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    const basketIconContent = document.querySelector(".basketIcon_content");
-    const closeBtn = document.querySelector(".close_btn");
-    const continueShoppingBtn = document.querySelector(".DialogContent_btn button:first-child");
-    const viewBasketBtn = document.querySelector(".DialogContent_btn button:last-child");
-
-    // Функция для открытия окна корзины
-    function openBasket(item) {
-        const gameName = item.querySelector('.game_title').textContent;
-        const gameImage = item.querySelector('.panel_icons img').src;
-        const gamePrice = item.querySelector('.panel_price-left').textContent;
-
-        document.querySelector('.DialogContent_title').innerText = gameName;
-        document.querySelector('.game_image').src = gameImage;
-        document.querySelector('._1f3PID2BXOm-jAr3LGxkW3').innerText = gamePrice;
-
-        basketIconContent.style.display = "block";
-    }
-
-    // Открытие окна при клике на игру
-    document.querySelectorAll('.basketIcon_inner').forEach(item => {
-        item.addEventListener('click', event => {
-            openBasket(item);
+                // Показываем окно с информацией
+                basketIcon.style.display = 'block';
+            });
         });
-    });
 
-    // Закрытие окна при нажатии на крестик
-    closeBtn.addEventListener("click", function () {
-        basketIconContent.style.display = "none";
-    });
-
-    // Закрытие окна при клике на кнопку "Continue shopping"
-    continueShoppingBtn.addEventListener("click", function () {
-        basketIconContent.style.display = "none";
-    });
-
-    // Переход к просмотру корзины при клике на кнопку "View basket"
-    viewBasketBtn.addEventListener("click", function () {
-        // Здесь можно добавить код для перехода на страницу корзины
-        // Например:
-        // window.location.href = "view_basket.html";
+        // Закрытие окна при клике вне него
+        document.addEventListener('click', (event) => {
+            if (!event.target.closest('.basketIcon') && !event.target.closest('.slide')) {
+                basketIcon.style.display = 'none'; // Добавлено закрытие окна корзины
+            }
+        });
     });
 });
